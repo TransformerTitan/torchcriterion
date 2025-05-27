c49806152527d1cb9ca7225296196226fd855b22
Transformer Titan <pyadla2@alumni.ncsu.edu>
2025-05-26 19:54:18 -0700

    Create huber.py

---
+++ regression/huber.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+
+class HuberLoss(nn.Module):
+    def __init__(self, delta=1.0):
+        super().__init__()
+        self.loss = nn.SmoothL1Loss(beta=delta)
+
+    def forward(self, inputs, targets):
+        return self.loss(inputs, targets)
