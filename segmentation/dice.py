bd2dcb01a6259fba5310bf07aa54aa13639ecd23
Transformer Titan <pyadla2@alumni.ncsu.edu>
2025-05-26 19:54:33 -0700

    Create dice.py

---
+++ segmentation/dice.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+class DiceLoss(nn.Module):
+    def __init__(self, smooth=1.0):
+        super().__init__()
+        self.smooth = smooth
+
+    def forward(self, inputs, targets):
+        inputs = torch.sigmoid(inputs)
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        intersection = (inputs * targets).sum()
+        return 1 - ((2. * intersection + self.smooth) / (inputs.sum() + targets.sum() + self.smooth))
